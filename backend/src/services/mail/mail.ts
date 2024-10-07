64123bba29f1517875e45a7aeafb699d3d258cdd
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-07 15:30:56 -0500

    funcion para enviar el email con el token de recuperacion de contraseña

--- backend/src/services/mail/mail.ts
+++ backend/src/services/mail/mail.ts
@@ -21,3 +21,19 @@ export async function sendConfirmEmail(user: IEmail) {
   });
   console.log(`Mensaje enviado ${info.messageId}`);
 }
+
+export async function sendResetPasswordEmail(user: IEmail) {
+  const info = await transporter.sendMail({
+    from: "Super Trivia <admin@superquiz.com>",
+    to: user.email,
+    subject: "Super Quiz - Recupera tu contraseña",
+    text: "Recupera tu contraseña en Super Quiz",
+    html: `<p>Hola ${user.name}, solo estás a un paso de recuparar tu contraseña en Super Quiz</p>
+          <p>Visita el siguiente enlace:</>
+          <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta">Confirmar Cuenta</a>
+          <p>E ingresa el siguiente código: <b>${user.token}</b></p>
+          <p>Este token expira en aproximadamente 2 horas</>
+    `,
+  });
+  console.log(`Mensaje enviado ${info.messageId}`);
+}
