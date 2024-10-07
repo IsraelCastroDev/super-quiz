9707affe3310c81e58cb08de5c353d3d357778ce
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-07 14:31:17 -0500

    función para enviar un email de confirmcaión

---
+++ backend/src/services/mail/mail.ts
@@ -0,0 +1,23 @@
+import { transporter } from "../../config/nodemailer";
+
+interface IEmail {
+  name: string;
+  email: string;
+  token: string;
+}
+
+export async function sendConfirmEmail(user: IEmail) {
+  const info = await transporter.sendMail({
+    from: "Super Trivia <admin@superquiz.com>",
+    to: user.email,
+    subject: "Super Quiz - Confirma tu cuenta",
+    text: "Confirma tu cuenta en Super Quiz",
+    html: `<p>Hola ${user.name}, solo estás a un paso de divertirte en Super Quiz, confirma tu cuenta</p>
+          <p>Visita el siguiente enlace:</>
+          <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta">Confirmar Cuenta</a>
+          <p>E ingresa el siguiente código: <b>${user.token}</b></p>
+          <p>Este token expira en aproximadamente 2 horas</>
+    `,
+  });
+  console.log(`Mensaje enviado ${info.messageId}`);
+}
