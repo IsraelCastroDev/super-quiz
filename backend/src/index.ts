2ef394b26ed64d5db6e6dbb20f29b505a21fbe69
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-07 14:29:41 -0500

    ejecutando funcion de cron job

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import server from "./server";
+import { scheduleCronJobs } from "./utils/cron-jobs";
 
 const PORT = process.env.PORT || 3000;
+
+scheduleCronJobs();
 
 server.listen(PORT, () =>
   console.log(`server running on http://localhost:${PORT}`)
