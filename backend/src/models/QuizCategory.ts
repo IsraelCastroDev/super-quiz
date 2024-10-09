2c90e5f2cecf91908f472f1a126e9f6aaebdd324
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-08 22:53:43 -0500

    borrando el created_at, ya que se crear por defecto

--- backend/src/models/QuizCategory.ts
+++ backend/src/models/QuizCategory.ts
@@ -2,7 +2,6 @@ import mongoose, { Document, Schema, Types } from "mongoose";
 
 export type QuizCategoryType = Document & {
   state: string;
-  created_at: Date;
   quiz: Types.ObjectId;
   category: Types.ObjectId;
 };
@@ -10,7 +9,6 @@ export type QuizCategoryType = Document & {
 const QuizCategorySchema: Schema = new Schema(
   {
     state: { type: String, required: true, trim: true },
-    created_at: { type: Date, required: true, trim: true },
     quiz: { type: Types.ObjectId, ref: "Quiz" },
     category: { type: Types.ObjectId, ref: "Category" },
   },
