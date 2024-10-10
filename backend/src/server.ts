d44ef6ce3b2245e5e99cfde0e12eaa9a971505ab
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-09 21:03:07 -0500

    configurando cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,15 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes";
 import quizRoutes from "./routes/quiz.routes";
+import cors from "cors";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors";
 
 const server = express();
 connectDB();
 
 server.use(express.json());
+server.use(cors(corsConfig));
 server.use(cookieParser());
 
 server.use("/api/v1/users", userRoutes);
