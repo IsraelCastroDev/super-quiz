f6c0231179932d94b100acb26e1555b59d8ba279
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-11 11:59:17 -0500

    función para generar un token alfanumerico

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,3 +1,15 @@
-export function generateToken() {
+export function generateToken(): string {
   return Math.floor(100000 + Math.random() * 900000).toString();
+}
+export function generateTokenQuiz(): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+  let token = "";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters[randomIndex];
+  }
+
+  return token;
 }
