fb6bf6310f0fdd91f222d5a6cae59bb12a91e903
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 13:55:09 -0500

    Creando layout del home

---
+++ frontend/src/layouts/HomeLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
+
+function HomeLayout() {
+  return (
+    <>
+      <Header />
+      <main className="px-2 py-4 md:px-14 md:py-8">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+export default HomeLayout;
