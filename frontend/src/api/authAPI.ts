ec719ea91efc84504276af3750f4a096d48c8796
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 16:18:59 -0500

    función para hacer la petición, login

--- frontend/src/api/authAPI.ts
+++ frontend/src/api/authAPI.ts
@@ -1,10 +1,24 @@
 import { isAxiosError } from "axios";
 import { api } from "./axiosConfig";
-import { UserFormData } from "@/types";
+import { UserFormData, UserLoginData } from "@/types";
 
 export const registerUser = async (formData: UserFormData) => {
   try {
     const { data } = await api.post("/users/create-account", {
+      ...formData,
+    });
+
+    return data.message;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data.message);
+    }
+  }
+};
+
+export const loginUser = async (formData: UserLoginData) => {
+  try {
+    const { data } = await api.post("/users/login", {
       ...formData,
     });
 
