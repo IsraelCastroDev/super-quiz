f3a81edecfbb894a6675badaa5f10bdd42fb08c0
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 15:23:18 -0500

    creando hook para separa la lógica de validación y registro

---
+++ frontend/src/hooks/useRegisterUser.ts
@@ -0,0 +1,46 @@
+import { registerUser } from "@/api/authAPI";
+import { useAppStore } from "@/store/useAppStore";
+import { UserFormData } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+export function useRegisterUser() {
+  const addNotification = useAppStore((state) => state.addNotification);
+  const navigate = useNavigate();
+
+  const mutationRegisterUser = useMutation({
+    mutationFn: registerUser,
+    onSuccess(data) {
+      addNotification({
+        title: data,
+        type: "success",
+      });
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    },
+    onError: (error) => {
+      addNotification({
+        title: error.message,
+        type: "error",
+      });
+    },
+  });
+
+  return mutationRegisterUser;
+}
+
+export function useValidationRegisterUserForm() {
+  const validationRegisterUserForm = useForm<UserFormData>({
+    defaultValues: {
+      name: "",
+      lastname: "",
+      username: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    },
+  });
+  return validationRegisterUserForm;
+}
