8657bd431d3e20338ee89dacf436c69d33f3aab7
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 13:56:29 -0500

    creando el type del usuario

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  name: z.string(),
+  lastname: z.string(),
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  confirm_password: z.string(),
+});
+
+export type UserFormData = z.infer<typeof UserSchema>;
