f7986d47862e94173c1bfe76394567f316072db9
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 15:23:46 -0500

    Optimizando componente

---
+++ frontend/src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,25 @@
+import Text from "@/components/ui/Typography";
+import RegisterForm from "@/components/auth/RegisterForm";
+import { useRegisterUser } from "@/hooks/useRegisterUser";
+import { UserFormData } from "@/types";
+
+function RegisterPage() {
+  const mutationRegisterUser = useRegisterUser();
+  const { mutate } = mutationRegisterUser;
+
+  const onSubmit = (data: UserFormData) => {
+    mutate(data);
+  };
+
+  return (
+    <section>
+      <div className="max-w-[35rem] mx-auto px-10 py-5 border border-slate-700 shadow-md rounded-md mt-5 space-y-3">
+        <Text as="h1" category="title" className="text-center">
+          Regístrate y crea tus propios Super Quiz 🚀
+        </Text>
+        <RegisterForm onSubmit={onSubmit} />
+      </div>
+    </section>
+  );
+}
+export default RegisterPage;
