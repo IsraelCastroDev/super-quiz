a828ad4b72a4553cc0af3b12061ae22a8b1c84d4
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 22:34:24 -0500

    creando ruta para confirmar cuenta y cambiar contraseña

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,17 +3,31 @@ import HomePage from "./pages/HomePage";
 import HomeLayout from "./layouts/HomeLayout";
 import RegisterPage from "./pages/auth/RegisterPage";
 import LoginPage from "./pages/auth/LoginPage";
+import SendEmailRecoverPasswordPage from "./pages/auth/SendEmailRecoverPasswordPage";
+import Notification from "./components/ui/Notification";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import ConfirmAccount from "./pages/auth/ConfirmAccount";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Notification />
         <Routes>
           <Route element={<HomeLayout />}>
             <Route path="/" element={<HomePage />} index />
             <Route path="/iniciar-sesion" element={<LoginPage />} />
             <Route path="/registrarse" element={<RegisterPage />} />
           </Route>
+          <Route path="/confirmar-cuenta" element={<ConfirmAccount />} />
+          <Route
+            path="/recuperar-cuenta"
+            element={<SendEmailRecoverPasswordPage />}
+          />
+          <Route
+            path="/recuperar-cuenta/cambiar-password"
+            element={<ResetPasswordPage />}
+          />
         </Routes>
       </BrowserRouter>
     </>
