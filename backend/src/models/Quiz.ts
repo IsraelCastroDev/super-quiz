4987c72a9b04b27a9e53ef57344943ef29511460
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 15:11:09 -0500

    Actualizando modelo Quiz

--- backend/src/models/Quiz.ts
+++ backend/src/models/Quiz.ts
@@ -1,39 +1,19 @@
 import mongoose, { Document, PopulatedDoc, Schema, Types } from "mongoose";
-import Question from "./Question";
-import Token from "./Token";
 
 export type QuizType = Document & {
   title: string;
   score?: number;
-  created_at: Date;
   user: Types.ObjectId;
-  questions: PopulatedDoc<QuizType & Document>[];
-  token?: string;
+  questions: Types.ObjectId[];
+  token: string;
 };
 
-const QuizSchema: Schema = new Schema(
-  {
-    title: { type: String, required: true, trim: true },
-    score: { type: Number, required: true, trim: true },
-    token: { type: String, trim: true },
-    user: { type: Types.ObjectId, ref: "User" },
-    questions: [{ type: Types.ObjectId, ref: "Question" }],
-  },
-  { timestamps: true }
-);
-
-QuizSchema.pre("findOneAndDelete", async function (next) {
-  const quizId = this.getQuery()["_id"];
-  console.log(quizId);
-
-  try {
-    await Question.deleteMany({ quiz: quizId });
-    await Token.deleteMany({ quiz: quizId });
-    next();
-  } catch (error) {
-    console.log(error);
-    next(error as Error);
-  }
+export const QuizSchema: Schema = new Schema({
+  title: { type: String, required: true, trim: true },
+  score: { type: Number, default: 0 },
+  user: { type: Types.ObjectId, ref: "User", required: true },
+  questions: [{ type: Types.ObjectId, ref: "Question" }],
+  token: { type: String, trim: true, required: true },
 });
 
 const Quiz = mongoose.model<QuizType>("Quiz", QuizSchema);
