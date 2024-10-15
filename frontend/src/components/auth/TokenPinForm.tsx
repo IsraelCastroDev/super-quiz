376baa79e635e49d77334d466a4e549caf7a0d6e
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 22:35:43 -0500

    creando componente para el pin

---
+++ frontend/src/components/auth/TokenPinForm.tsx
@@ -0,0 +1,49 @@
+import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import Text from "@/components/ui/Typography";
+import { useValidateToken } from "@/hooks/useLoginUser";
+import { useAppStore } from "@/store/useAppStore";
+
+function TokenPinForm() {
+  const { mutate } = useValidateToken();
+  const tokenResetPassword = useAppStore((state) => state.tokenResetPassword);
+  const setTokenResetPassword = useAppStore(
+    (state) => state.setTokenResetPassword
+  );
+
+  const handleChange = (value: string) => setTokenResetPassword(value);
+
+  const handleComplete = (value: string) => {
+    mutate(value);
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen gap-4">
+      <div className="flex flex-col gap-3">
+        <div className="space-y-3">
+          <Text as="h1" category="brand" className="text-center">
+            Super Quiz
+          </Text>
+          <Text as="h2" category="title">
+            Ingresa el token que te enviamos a tu correo
+          </Text>
+        </div>
+
+        <div className="flex items-center justify-center gap-5">
+          <PinInput
+            value={tokenResetPassword ? tokenResetPassword : ""}
+            onChange={handleChange}
+            onComplete={handleComplete}
+          >
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+            <PinInputField className="w-10 md:w-14 border border-slate-700 p-3 text-center" />
+          </PinInput>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default TokenPinForm;
