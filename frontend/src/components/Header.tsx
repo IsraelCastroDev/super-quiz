05571d9e48cba1f166a779fde9010cfefe299b89
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 10:32:48 -0500

    usando los datos de usuario que inició sesión

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,20 +1,43 @@
+import { useLogoutUser } from "@/hooks/useAuthUser";
+import { useAppPersists } from "@/store/useAppPersists";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
+import Loader from "./ui/Loader/Loader";
 
 function Header() {
+  const userAuth = useAppPersists((state) => state.userAuth);
+
+  const { mutate, isPending } = useLogoutUser();
+
+  const handleLogout = () => mutate();
+
   return (
-    <header className="py-2 px-14 flex flex-col md:flex-row justify-between items-center sticky top-0 z-10 bg-gray-200/50 backdrop-blur">
-      <Link to={"/"} className="text-3xl font-bold">
-        Super Quiz
-      </Link>
-      <div className="flex items-center gap-4">
-        <Link to={"/iniciar-sesion"} className={"link"}>
-          Iniciar Sesión
+    <>
+      {isPending && <Loader />}
+      <header className="py-2 px-14 flex flex-col md:flex-row justify-between items-center sticky top-0 z-10 bg-gray-200/50 backdrop-blur">
+        <Link to={"/"} className="text-3xl font-bold">
+          Super Quiz
         </Link>
-        <Link to={"/registrarse"} className={"link"}>
-          Registrarse
-        </Link>
-      </div>
-    </header>
+        {userAuth ? (
+          <nav>
+            <div className="flex items-center gap-2">
+              <UserCircleIcon className="w-10 h-10" />
+              <h3 className="text-lg font-bold">{userAuth.name}</h3>
+              <button onClick={handleLogout}>Cerrar sesión</button>
+            </div>
+          </nav>
+        ) : (
+          <nav className="flex items-center gap-4">
+            <Link to={"/iniciar-sesion"} className={"link"}>
+              Iniciar Sesión
+            </Link>
+            <Link to={"/registrarse"} className={"link"}>
+              Registrarse
+            </Link>
+          </nav>
+        )}
+      </header>
+    </>
   );
 }
 export default Header;
