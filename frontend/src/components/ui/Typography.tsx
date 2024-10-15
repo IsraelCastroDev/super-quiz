23c70264a54c3396610c606bd9549881e9a64aeb
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 22:36:11 -0500

    agregando una categoría más

--- frontend/src/components/ui/Typography.tsx
+++ frontend/src/components/ui/Typography.tsx
@@ -1,5 +1,5 @@
 type HtmlTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-type Category = "title" | "subtitle" | "body";
+type Category = "title" | "subtitle" | "body" | "brand";
 
 interface TextProps {
   as: HtmlTag;
@@ -12,6 +12,10 @@ function Text({ as: Element, category, children, className }: TextProps) {
   let baseClass = "";
 
   switch (category) {
+    case "brand":
+      baseClass =
+        "text-5xl md:text-6xl font-bold bg-gradient-to-tl from-gray-950 via-sky-800 to-sky-950 bg-clip-text text-transparent";
+      break;
     case "title":
       baseClass = "text-2xl md:text-3xl font-bold";
       break;
