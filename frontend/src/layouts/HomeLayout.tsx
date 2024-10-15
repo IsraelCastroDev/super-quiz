0081653cf61bd7522ba8be4026b0ec52fba161ec
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 11:39:28 -0500

    quitando componente innceseario

--- frontend/src/layouts/HomeLayout.tsx
+++ frontend/src/layouts/HomeLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import Notification from "../components/ui/Notification";
 
 function HomeLayout() {
   return (
     <>
-      <Notification />
       <Header />
       <main className="px-2 py-4 md:px-14 md:py-8">
         <Outlet />
