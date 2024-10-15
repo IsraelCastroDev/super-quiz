2c7d1d25bb539e050a31290faf356164177ec647
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 15:11:35 -0500

    corrigiendo validacion

--- backend/src/routes/quiz.routes.ts
+++ backend/src/routes/quiz.routes.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 import { QuizController } from "../controllers/QuizController";
 import { authenticate } from "../middlewares/authenticate.middleware";
 import { body, param } from "express-validator";
-import { QuestionType } from "../models/Question";
-import { CategoryType } from "../models/Category";
+import { AnswerType } from "../models/Answer";
 import { validateErrors } from "../middlewares/validateErrors.middleware";
+import { QuestionType } from "../models/Question";
 
 const router = Router();
 
@@ -13,16 +13,16 @@ router.post(
   authenticate,
   body("title")
     .notEmpty()
-    .withMessage("El titúlo es requerido")
+    .withMessage("El título es requerido")
     .isString()
-    .withMessage("El titúlo debe ser una cadena de texto"),
+    .withMessage("El título debe ser una cadena de texto"),
   body("categories")
     .notEmpty()
     .withMessage("Las categorías son requeridas")
     .isArray()
     .withMessage("Las categorías deben ser un arreglo")
     .custom((value) => {
-      value.forEach((category: CategoryType) => {
+      value.forEach((category: string) => {
         if (typeof category !== "string") {
           throw new Error("Cada categoría debe ser una cadena de texto");
         }
@@ -36,19 +36,42 @@ router.post(
     .withMessage("Las preguntas deben ser un arreglo")
     .isLength({ min: 5, max: 20 })
     .withMessage("El quiz debe tener entre 5 y 20 preguntas")
-    .custom((values) => {
-      values.forEach((question: QuestionType) => {
+    .custom((questions) => {
+      questions.forEach((question: QuestionType) => {
         if (
           typeof question !== "object" ||
-          !question.name ||
-          typeof question.name !== "string" ||
-          typeof question.is_correct !== "boolean"
+          !question.title ||
+          typeof question.title !== "string"
         ) {
           throw new Error(
-            "Cada pregunta debe ser un objeto con propiedades 'name' (string) e 'is_correct' (boolean)"
+            "Cada pregunta debe ser un objeto con la propiedad 'title' (string)"
           );
         }
+
+        if (!question.answers || !Array.isArray(question.answers)) {
+          throw new Error(
+            "Cada pregunta debe contener un arreglo de respuestas"
+          );
+        }
+
+        if (question.answers.length !== 5) {
+          throw new Error("Cada pregunta debe tener exactamente 5 respuestas");
+        }
+
+        question.answers.forEach((answer: any) => {
+          if (
+            typeof answer !== "object" ||
+            !answer.name ||
+            typeof answer.name !== "string" ||
+            typeof answer.is_correct !== "boolean"
+          ) {
+            throw new Error(
+              "Cada respuesta debe ser un objeto con las propiedades 'name' (string) e 'is_correct' (boolean)"
+            );
+          }
+        });
       });
+
       return true;
     }),
   validateErrors,
