918d75020239f1dc4115d4d34c34775e1e5205cd
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-14 22:38:16 -0500

    agregando slice

--- frontend/src/store/useAppStore.ts
+++ frontend/src/store/useAppStore.ts
@@ -3,7 +3,11 @@ import {
   notificationSlice,
   NotificationSliceType,
 } from "./slices/notificationSlice";
+import { stateAppSlice, StateAppType } from "./slices/stateAppSlice";
 
-export const useAppStore = create<NotificationSliceType>((...a) => ({
-  ...notificationSlice(...a),
-}));
+export const useAppStore = create<NotificationSliceType & StateAppType>(
+  (...a) => ({
+    ...notificationSlice(...a),
+    ...stateAppSlice(...a),
+  })
+);
