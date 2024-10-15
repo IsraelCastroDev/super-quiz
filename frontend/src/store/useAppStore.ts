b8ea85ed201c2a4859d78b05692184cd33aff33a
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 09:17:33 -0500

    agregando las devtools de zustand

--- frontend/src/store/useAppStore.ts
+++ frontend/src/store/useAppStore.ts
@@ -1,13 +1,14 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import {
   notificationSlice,
   NotificationSliceType,
 } from "./slices/notificationSlice";
 import { stateAppSlice, StateAppType } from "./slices/stateAppSlice";
 
-export const useAppStore = create<NotificationSliceType & StateAppType>(
-  (...a) => ({
+export const useAppStore = create<NotificationSliceType & StateAppType>()(
+  devtools((...a) => ({
     ...notificationSlice(...a),
     ...stateAppSlice(...a),
-  })
+  }))
 );
