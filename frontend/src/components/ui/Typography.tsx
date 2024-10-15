4795e5364ac95740ef038d6f6b9d3614bc0fe8b8
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-15 11:51:33 -0500

    mejorando componente

--- frontend/src/components/ui/Typography.tsx
+++ frontend/src/components/ui/Typography.tsx
@@ -1,29 +1,40 @@
 type HtmlTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-type Category = "title" | "subtitle" | "body" | "brand";
+type Category = "title" | "subtitle" | "body" | "big";
 
 interface TextProps {
   as: HtmlTag;
   category: Category;
   children: React.ReactNode;
   className?: string;
+  degraded?: boolean;
 }
 
-function Text({ as: Element, category, children, className }: TextProps) {
+function Text({
+  as: Element,
+  category,
+  children,
+  className,
+  degraded = false,
+}: TextProps) {
   let baseClass = "";
+  const degradedClass = `${
+    degraded
+      ? "bg-gradient-to-tl from-gray-950 via-sky-800 to-sky-950 bg-clip-text text-transparent"
+      : ""
+  }`;
 
   switch (category) {
-    case "brand":
-      baseClass =
-        "text-5xl md:text-6xl font-bold bg-gradient-to-tl from-gray-950 via-sky-800 to-sky-950 bg-clip-text text-transparent";
+    case "big":
+      baseClass = `${degradedClass} text-5xl md:text-6xl font-bold`;
       break;
     case "title":
-      baseClass = "text-2xl md:text-3xl font-bold";
+      baseClass = `${degradedClass} text-2xl md:text-3xl font-bold`;
       break;
     case "subtitle":
-      baseClass = "text-xl md:text-2xl font-bold";
+      baseClass = `${degradedClass} text-lg md:text-xl font-bold`;
       break;
     case "body":
-      baseClass = "text-sm md:text-base font-normal";
+      baseClass = `${degradedClass} text-sm md:text-base font-normal`;
       break;
 
     default:
