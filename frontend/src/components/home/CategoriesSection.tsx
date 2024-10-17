d5c870975125abd52ea6d41098f8f1ff99dcab79
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-16 19:09:38 -0500

    uasndo el componente container

--- frontend/src/components/home/CategoriesSection.tsx
+++ frontend/src/components/home/CategoriesSection.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import Text from "../ui/Typography";
+import Loader from "../ui/Loader/Loader";
 import { getQuizCategories } from "@/api/quizAPI";
 import { Link } from "react-router-dom";
-import Loader from "../ui/Loader/Loader";
 import { convertStringToSlug } from "@/utils";
+import Container from "../ui/app/Container";
 
 function CategoriesSection() {
   const { data: categories, isLoading } = useQuery({
@@ -12,45 +13,52 @@ function CategoriesSection() {
   });
 
   return (
-    <section className="mt-24 relative">
-      {isLoading && <Loader />}
-      <div>
-        <Text as="h2" category="subtitle">
-          Todas las categorías
-        </Text>
-      </div>
+    <Container>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div>
+            <Text as="h2" category="subtitle">
+              Todas las categorías
+            </Text>
+          </div>
 
-      <div>
-        <ul className="mt-5 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5 bg-slate-100 p-5 rounded">
-          {categories && categories.length > 0 ? (
-            categories.map((category) => (
-              <li
-                key={category._id}
-                className="border border-slate-700 p-5 rounded"
-              >
-                <Link
-                  to={`/quizzes/category/${convertStringToSlug(category.name)}`}
-                >
+          <div>
+            <ul className="mt-5 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5 bg-slate-100 p-5 rounded">
+              {categories && categories.length > 0 ? (
+                categories.map((category) => (
+                  <li
+                    key={category._id}
+                    className="border border-slate-700 p-5 rounded"
+                  >
+                    <Link
+                      to={`/quizzes/category/${convertStringToSlug(
+                        category.name
+                      )}`}
+                    >
+                      <Text as="p" category="subtitle">
+                        {category.name}
+                      </Text>
+                    </Link>
+
+                    <Text as="p" category="body">
+                      {category.description}
+                    </Text>
+                  </li>
+                ))
+              ) : (
+                <li>
                   <Text as="p" category="subtitle">
-                    {category.name}
+                    Aún no hay categorías
                   </Text>
-                </Link>
-
-                <Text as="p" category="body">
-                  {category.description}
-                </Text>
-              </li>
-            ))
-          ) : (
-            <li>
-              <Text as="p" category="subtitle">
-                Aún no hay categorías
-              </Text>
-            </li>
-          )}
-        </ul>
-      </div>
-    </section>
+                </li>
+              )}
+            </ul>
+          </div>
+        </>
+      )}
+    </Container>
   );
 }
 export default CategoriesSection;
