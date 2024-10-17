cf998d8ebb4c10f7b05ce0222a070904ee32a21a
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-17 12:10:26 -0500

    agrupando importaciones

---
+++ frontend/src/components/ui/Form/index.ts
@@ -0,0 +1,4 @@
+export { ButtonSubmit } from "./ButtonSubmit";
+export { Form } from "./Form";
+export { InputField } from "./InputField";
+export { Label } from "./Label";
---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./Button";
+export { BackLink } from "./BackLink";
+export { ErrorMessage } from "./ErrorMessage";
+export { MenuProfile } from "./MenuProfile";
+export { Notification } from "./Notification";
+export { Text } from "./Typography";
+export { Loader } from "./Loader/Loader";
