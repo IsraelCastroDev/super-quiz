04cc09994b067f9b2c54957e7e00aa17ce251d96
Isreal Castro <israelcastro.dev@gmail.com>
2024-10-16 19:56:49 -0500

    usando el componente Text

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Loader from "./ui/Loader/Loader";
 import { useState } from "react";
 import MenuProfile from "./ui/MenuProfile";
+import Text from "./ui/Typography";
 
 function Header() {
   const userAuth = useAppPersists((state) => state.userAuth);
@@ -12,45 +13,54 @@ function Header() {
 
   const { mutate, isPending } = useLogoutUser();
 
-  const handleShowProfileMenu = () => setShowProfileMenu(!showProfileMenu);
+  const handleShowProfileMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setShowProfileMenu(!showProfileMenu);
+  };
   const handleLogout = () => mutate();
 
   return (
     <>
       {isPending && <Loader />}
-      <header className="py-2 px-14 flex flex-col md:flex-row justify-between items-center sticky top-0 z-10 bg-gray-200/50 backdrop-blur-sm border border-slate-700 rounded-md">
-        <Link to={"/"} className="text-3xl font-bold">
-          Super Quiz
-        </Link>
-        {userAuth ? (
-          <nav>
-            <div className="flex items-center gap-2">
-              <button onClick={handleShowProfileMenu}>
-                <UserCircleIcon className="w-10 h-10" />
-              </button>
-              {showProfileMenu && (
-                <MenuProfile
-                  handleLogout={handleLogout}
-                  setShowProfileMenu={setShowProfileMenu}
-                />
-              )}
-
+      <div className="relative">
+        <header className="py-2 px-5 md:px-16 flex flex-row justify-between items-center sticky top-0 w-full z-10 bg-gray-200/50 backdrop-blur-sm border border-slate-700 rounded-md">
+          <Link to={"/"}>
+            <Text as="h1" category="title">
+              Super Quiz
+            </Text>
+          </Link>
+          {userAuth ? (
+            <nav>
               <div className="flex items-center gap-2">
-                <h3 className="text-lg font-bold">{userAuth.username}</h3>
+                <button onClick={handleShowProfileMenu}>
+                  <UserCircleIcon className="w-10 h-10" />
+                </button>
+                {showProfileMenu && (
+                  <MenuProfile
+                    handleLogout={handleLogout}
+                    setShowProfileMenu={setShowProfileMenu}
+                  />
+                )}
+
+                <div className="flex items-center gap-2">
+                  <Text as="h3" category="body" className="font-black">
+                    {userAuth.username}
+                  </Text>
+                </div>
               </div>
-            </div>
-          </nav>
-        ) : (
-          <nav className="flex items-center gap-4">
-            <Link to={"/iniciar-sesion"} className={"link"}>
-              Iniciar Sesión
-            </Link>
-            <Link to={"/registrarse"} className={"link"}>
-              Registrarse
-            </Link>
-          </nav>
-        )}
-      </header>
+            </nav>
+          ) : (
+            <nav className="flex items-center gap-4">
+              <Link to={"/iniciar-sesion"} className={"link"}>
+                Iniciar Sesión
+              </Link>
+              <Link to={"/registrarse"} className={"link"}>
+                Registrarse
+              </Link>
+            </nav>
+          )}
+        </header>
+      </div>
     </>
   );
 }
