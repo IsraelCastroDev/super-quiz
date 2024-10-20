019eae59aef242e2e3e7d67799e1f6dbdba82951
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-20 11:17:07 -0500

    poniendo en un variable de entorno la url del backend

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: import.meta.env.VITE_URL_BACKEND,
   withCredentials: true,
 });
