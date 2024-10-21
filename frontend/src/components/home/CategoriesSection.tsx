0b7dee974d5262eb86baf9cae3c006b900801f6c
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-21 13:22:12 -0500

    ajustando HTML y el estado de carga

--- frontend/src/components/home/CategoriesSection.tsx
+++ frontend/src/components/home/CategoriesSection.tsx
@@ -13,50 +13,48 @@ export function CategoriesSection() {
 
   return (
     <Container>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <div>
-            <Text as="h2" category="subtitle">
-              Todas las categorías
-            </Text>
-          </div>
+      <div>
+        <Text as="h2" category="subtitle">
+          Todas las categorías
+        </Text>
+      </div>
 
-          <div>
-            <ul className="mt-5 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5 bg-slate-100 p-5 rounded">
-              {categories && categories.length > 0 ? (
-                categories.map((category) => (
-                  <li
-                    key={category._id}
-                    className="border border-slate-700 p-5 rounded"
+      <div className="relative aspect-video">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <ul className="mt-5 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5 bg-slate-100 p-5 rounded">
+            {categories && categories.length > 0 ? (
+              categories.map((category) => (
+                <li
+                  key={category._id}
+                  className="border border-slate-700 p-5 rounded"
+                >
+                  <Link
+                    to={`/quizzes/category/${convertStringToSlug(
+                      category.name
+                    )}`}
                   >
-                    <Link
-                      to={`/quizzes/category/${convertStringToSlug(
-                        category.name
-                      )}`}
-                    >
-                      <Text as="p" category="subtitle">
-                        {category.name}
-                      </Text>
-                    </Link>
-
-                    <Text as="p" category="body">
-                      {category.description}
+                    <Text as="p" category="subtitle">
+                      {category.name}
                     </Text>
-                  </li>
-                ))
-              ) : (
-                <li>
-                  <Text as="p" category="subtitle">
-                    Aún no hay categorías
+                  </Link>
+
+                  <Text as="p" category="body">
+                    {category.description}
                   </Text>
                 </li>
-              )}
-            </ul>
-          </div>
-        </>
-      )}
+              ))
+            ) : (
+              <li>
+                <Text as="p" category="subtitle">
+                  Aún no hay categorías
+                </Text>
+              </li>
+            )}
+          </ul>
+        )}
+      </div>
     </Container>
   );
 }
