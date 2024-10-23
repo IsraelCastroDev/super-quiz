ea9a8d120de23b641536f06a9229d259c5434604
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 13:06:25 -0500

    ajustando css del loader, ajustando la posicion

--- frontend/src/components/ui/Loader/Loader.tsx
+++ frontend/src/components/ui/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import styles from "./Loader.module.css";
 
 export function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center absolute top-1/2 right-1/2">
       <div className={styles.loader}></div>
     </div>
   );
