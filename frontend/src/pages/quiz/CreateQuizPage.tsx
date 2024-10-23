05146b2eb7e9ed51627a4168abb6967b99badfe2
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 13:07:12 -0500

    página para crear un quiz

--- frontend/src/pages/quiz/CreateQuizPage.tsx
+++ frontend/src/pages/quiz/CreateQuizPage.tsx
@@ -1,3 +1,25 @@
+import { Text } from "@/components/ui";
+import { ContainerForm } from "@/components/ui/Form";
+import { useCategoriesQuiz } from "@/hooks/useCreateQuiz";
+import { CreateQuizForm } from "@/components/CreateQuiz";
+
 export function CreateQuizPage() {
-  return <div>CreateQuizPage</div>;
+  const { data: categories, isPending } = useCategoriesQuiz();
+
+  return (
+    <>
+      <div>
+        <Text as="h1" category="title" className="text-center">
+          Crear Super Quiz
+        </Text>
+      </div>
+
+      <ContainerForm>
+        <Text as="h2" category="subtitle">
+          Ingresa los datos necesarios
+        </Text>
+        <CreateQuizForm categories={categories} isPending={isPending} />
+      </ContainerForm>
+    </>
+  );
 }
