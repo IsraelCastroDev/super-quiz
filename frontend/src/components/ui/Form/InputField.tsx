4d0767aa8f55ae5095fa9fce747e4d55e1541d25
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 13:05:38 -0500

    haciendo el componente mas flexible

--- frontend/src/components/ui/Form/InputField.tsx
+++ frontend/src/components/ui/Form/InputField.tsx
@@ -1,15 +1,18 @@
 import { InputHTMLAttributes } from "react";
-import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+import { FieldError, Merge, UseFormRegisterReturn } from "react-hook-form";
 import { Label } from "@/components/ui/Form";
 import { ErrorMessage } from "@/components/ui";
 
+type BorderStyle = "slate" | "green";
+
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
   id: string;
-  error?: FieldError;
-  register: UseFormRegisterReturn;
+  error?: Merge<FieldError, undefined>;
+  register?: UseFormRegisterReturn;
   IconComponent?: React.ElementType;
   onIconClick?: () => void;
+  borderStyle?: BorderStyle;
 }
 
 export function InputField({
@@ -19,15 +22,20 @@ export function InputField({
   register,
   IconComponent,
   onIconClick,
+  borderStyle = "slate",
   ...props
 }: Props) {
   return (
-    <div className="flex flex-col gap-2">
-      <Label htmlFor={id} text={label} />
-      <div className="flex items-center justify-between  border border-slate-700 shadow-sm rounded-md w-full px-1">
+    <div className="flex flex-col gap-2 w-full">
+      {label && <Label htmlFor={id} text={label} />}
+      <div
+        className={`flex items-center justify-between border shadow-sm rounded-md w-full ${
+          borderStyle === "slate" ? "border-slate-700" : "border-green-400"
+        }`}
+      >
         <input
           id={id}
-          className="p-2 w-full outline-none"
+          className={`p-2 w-full outline-none`}
           {...register}
           {...props}
         />
