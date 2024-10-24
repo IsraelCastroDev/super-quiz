2148c8e81538a17d338b74a4d601ee90a4cebdcc
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 21:52:16 -0500

    función para formatear fecha

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -5,3 +5,14 @@ export const convertStringToSlug = (str: string): string => {
     .toLowerCase()
     .replace(/\s+/g, "_");
 };
+
+export const formatDate = (dateStr: string): string => {
+  const date = new Date(dateStr);
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const hours = date.getUTCHours().toString().padStart(2, "0");
+  const minutes = date.getUTCMinutes().toString().padStart(2, "0");
+
+  return `${day}/${month}/${year} - ${hours}:${minutes}`;
+};
