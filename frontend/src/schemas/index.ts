f9b0bc1c6bea5e358cdeecffd2dd7417e3d81d57
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 21:51:48 -0500

    creando schema para validar el llamado a la api . perfil de usuario

--- frontend/src/schemas/index.ts
+++ frontend/src/schemas/index.ts
@@ -7,3 +7,11 @@ export const QuizCategorySchema = z.object({
 });
 
 export const QuizCategoryAPIResponseSchema = z.array(QuizCategorySchema);
+
+export const UserProfileAPIResponseSchema = z.object({
+  name: z.string(),
+  lastname: z.string(),
+  username: z.string(),
+  email: z.string(),
+  created_at: z.string(),
+});
