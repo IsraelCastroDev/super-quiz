a25f0693b505c22cd73f5a7865f1d2171d467ca5
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-24 00:00:23 -0500

    agregando exportaciones

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,13 @@
+export {
+  confirmAccount,
+  getUserProfile,
+  loginUser,
+  logoutUser,
+  recoverPassword,
+  registerUser,
+  resetPassword,
+  validateAuth,
+  validateToken,
+} from "./authAPI";
+
+export { createQuiz, getQuizCategories, getQuizFromUser } from "./quizAPI";
