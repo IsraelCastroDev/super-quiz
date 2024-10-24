91b98d50a8aba7faeb65d6a070bfecaa4de37519
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-24 13:02:26 -0500

    agregando un campo slug para en el modelo

--- backend/src/models/Category.ts
+++ backend/src/models/Category.ts
@@ -2,11 +2,13 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export type CategoryType = Document & {
   name: string;
+  slug: string;
   description: string;
 };
 
 const CategorySchema: Schema = new Schema({
   name: { type: String, required: true, trim: true },
+  slug: { type: String, require: true, trim: true },
   description: { type: String, required: true, trim: true },
 });
 
