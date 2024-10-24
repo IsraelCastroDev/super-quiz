8fa81b062bd496096a1b0ca7eedea12abb03b63f
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-23 23:58:54 -0500

    agregando indice

--- backend/src/models/QuizCategory.ts
+++ backend/src/models/QuizCategory.ts
@@ -1,19 +1,19 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
 
 export type QuizCategoryType = Document & {
-  state: string;
   quiz: Types.ObjectId;
   category: Types.ObjectId;
 };
 
 const QuizCategorySchema: Schema = new Schema(
   {
-    state: { type: String, required: true, trim: true },
-    quiz: { type: Types.ObjectId, ref: "Quiz" },
-    category: { type: Types.ObjectId, ref: "Category" },
+    quiz: { type: Types.ObjectId, ref: "Quiz", required: true },
+    category: { type: Types.ObjectId, ref: "Category", required: true },
   },
   { timestamps: true }
 );
+
+QuizCategorySchema.index({ quiz: 1, category: 1 }, { unique: true });
 
 const QuizCategory = mongoose.model<QuizCategoryType>(
   "QuizCategory",
