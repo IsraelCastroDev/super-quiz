dbcb4a0bc14363959fe98d83ae86074e5ea3f23f
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-24 00:17:12 -0500

    renderizando footer

--- frontend/src/layouts/HomeLayout.tsx
+++ frontend/src/layouts/HomeLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export function HomeLayout() {
   return (
@@ -8,6 +9,7 @@ export function HomeLayout() {
       <main className="px-2 py-4 md:px-0 md:py-8">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
