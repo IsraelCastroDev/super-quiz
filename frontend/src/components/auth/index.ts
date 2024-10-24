14e514aa35a74270b1944a3b78c9c319a5b3b2bb
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-24 00:01:00 -0500

    agregando exportaciones

--- frontend/src/components/auth/index.ts
+++ frontend/src/components/auth/index.ts
@@ -3,3 +3,5 @@ export { ProtectedRoute } from "./ProtectedRoute";
 export { RegisterForm } from "./RegisterForm";
 export { ResetPasswordForm } from "./ResetPasswordForm";
 export { TokenPinForm } from "./TokenPinForm";
+export { UserProfileData } from "./profile/UserProfileData";
+export { UserProfileQuizzes } from "./profile/UserProfileQuizzes";
