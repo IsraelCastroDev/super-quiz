46a142a8bd314513534524630baf9d72dd01ed01
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-24 00:01:47 -0500

    ajustando css

--- frontend/src/components/ui/app/Container.tsx
+++ frontend/src/components/ui/app/Container.tsx
@@ -2,12 +2,15 @@ import { useLocation } from "react-router-dom";
 
 interface Props {
   children: React.ReactNode;
+  styles?: string;
 }
 
-export function Container({ children }: Props) {
+export function Container({ children, styles }: Props) {
   const { pathname } = useLocation();
   return (
-    <section className={`${pathname === "/" ? "mt-24" : "mt-4"} relative`}>
+    <section
+      className={`${pathname === "/" ? "mt-24" : "mt-4"} relative ${styles}`}
+    >
       {children}
     </section>
   );
