853c4fd42bf42c271abcc6b339df646f3e7388c1
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-10-27 20:35:35 -0500

    modificando cors

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -8,7 +8,7 @@ export const corsConfig: CorsOptions = {
       whiteList.push(undefined);
     }
 
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error("No permitido por CORS"));
