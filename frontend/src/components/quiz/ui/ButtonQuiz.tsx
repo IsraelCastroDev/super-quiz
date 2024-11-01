4d2f2174ee4ce0303629bec86e19178a313b734a
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-01 13:56:24 -0500

    creando componente button quiz

---
+++ frontend/src/components/quiz/ui/ButtonQuiz.tsx
@@ -0,0 +1,9 @@
+export function ButtonQuiz() {
+  return (
+    <div className="mt-8 text-center">
+      <button className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105">
+        Iniciar Quiz
+      </button>
+    </div>
+  );
+}
