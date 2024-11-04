21abf038d4ddb7ab3ee6fc6a757220124b61da34
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 18:54:24 -0500

    agregando funcionalidad para evitar recargar la pagina por accidente

--- frontend/src/pages/quiz/QuizGamePage.tsx
+++ frontend/src/pages/quiz/QuizGamePage.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export function QuizGame() {
+  useEffect(() => {
+    const handleBeforeUnLoad = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnLoad);
+
+    return () => window.removeEventListener("beforeunload", handleBeforeUnLoad);
+  }, []);
+
   return (
     <>
       <h2>quiz game</h2>
