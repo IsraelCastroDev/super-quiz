420d136470f4fa393c5091fa30ac7cf94d59d733
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 16:38:53 -0500

    agregando la propiedad token

--- backend/src/controllers/QuizController.ts
+++ backend/src/controllers/QuizController.ts
@@ -143,6 +143,7 @@ export class QuizController {
               id: q._id,
             })),
             duration: quiz.duration,
+            token: quiz.token,
           };
         })
       );
