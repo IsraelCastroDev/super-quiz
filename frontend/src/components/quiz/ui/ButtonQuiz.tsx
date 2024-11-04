7ac70a2595cd5587ba61f08382b520cd8ed65a42
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 15:20:57 -0500

    agregando el onclick

--- frontend/src/components/quiz/ui/ButtonQuiz.tsx
+++ frontend/src/components/quiz/ui/ButtonQuiz.tsx
@@ -1,7 +1,15 @@
-export function ButtonQuiz() {
+interface Props {
+  handleShowQuiz?: () => void;
+}
+
+export function ButtonQuiz({ handleShowQuiz }: Props) {
   return (
     <div className="mt-8 text-center">
-      <button className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105">
+      <button
+        onClick={handleShowQuiz}
+        type="button"
+        className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105"
+      >
         Iniciar Quiz
       </button>
     </div>
