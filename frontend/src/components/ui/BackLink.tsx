edc15f7ffb9ca983266362e220bd76a59bd28a29
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 17:51:03 -0500

    poniendo z-20

--- frontend/src/components/ui/BackLink.tsx
+++ frontend/src/components/ui/BackLink.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function BackLink() {
   return (
-    <div className="absolute top-10 left-4">
+    <div className="absolute top-10 left-4 z-20">
       <Link
         to={"/"}
         className="bg-slate-700 hover:bg-slate-800 text-white p-3 rounded"
