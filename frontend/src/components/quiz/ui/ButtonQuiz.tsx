6398e036b39d89cb177abb75ee0042c19ad49f63
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 17:51:20 -0500

    actualizando el nombre del prop

--- frontend/src/components/quiz/ui/ButtonQuiz.tsx
+++ frontend/src/components/quiz/ui/ButtonQuiz.tsx
@@ -1,12 +1,12 @@
 interface Props {
-  handleShowQuiz?: () => void;
+  handleShowDialogConfirm: () => void;
 }
 
-export function ButtonQuiz({ handleShowQuiz }: Props) {
+export function ButtonQuiz({ handleShowDialogConfirm }: Props) {
   return (
     <div className="mt-8 text-center">
       <button
-        onClick={handleShowQuiz}
+        onClick={handleShowDialogConfirm}
         type="button"
         className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105"
       >
