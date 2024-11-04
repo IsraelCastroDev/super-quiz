99582c873f353d42fa8de0ce21adf4cbc42a0b9f
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 15:21:47 -0500

    haciendo las consultas y haciendo el renderizado condicional

--- frontend/src/pages/quiz/QuizPage.tsx
+++ frontend/src/pages/quiz/QuizPage.tsx
@@ -1,9 +1,48 @@
+import { getCategorieById, getQuizById } from "@/api/quizAPI";
 import { QuizPresentation } from "@/components/quiz/QuizGame";
+import { Loader } from "@/components/ui";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export function QuizPage() {
+  const [showQuiz, setShowQuiz] = useState(false);
+
+  const { quizId } = useParams<{ quizId: string }>();
+
+  const { data: quiz, isLoading } = useQuery({
+    queryKey: ["quizDetail", quizId],
+    queryFn: () => getQuizById(quizId),
+    retry: 0,
+  });
+
+  const { data: quizCategories } = useQuery({
+    queryKey: ["getCategorie", quiz?._id],
+    queryFn: async () => {
+      if (!quiz || !quiz.categories) return [];
+
+      const promises = quiz.categories.map((categorie) =>
+        getCategorieById(categorie.category)
+      );
+      return Promise.all(promises);
+    },
+    enabled: !!quiz,
+    retry: 0,
+  });
+
+  const handleShowQuiz = () => setShowQuiz(true);
+
   return (
     <section>
-      <QuizPresentation />
+      {!showQuiz && isLoading ? (
+        <Loader />
+      ) : (
+        <QuizPresentation
+          handleShowQuiz={handleShowQuiz}
+          quiz={quiz}
+          categories={quizCategories}
+        />
+      )}
     </section>
   );
 }
