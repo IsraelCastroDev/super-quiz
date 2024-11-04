d2cda5e1a2dcf2ab12ac68fb806a386f6f4dcf4d
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-04 16:39:26 -0500

    ajustando props y css

--- frontend/src/components/quiz/CreateQuiz/QuizCode.tsx
+++ frontend/src/components/quiz/CreateQuiz/QuizCode.tsx
@@ -2,11 +2,14 @@ import { useAppStore } from "@/store";
 import { Text } from "@components/ui";
 import { ClipboardIcon } from "@heroicons/react/24/solid";
 
+type Variant = "gray" | "slate";
+
 interface Props {
   quizCode: string;
+  variant?: Variant;
 }
 
-export function QuizCode({ quizCode }: Props) {
+export function QuizCode({ quizCode, variant = "gray" }: Props) {
   const addNotification = useAppStore((state) => state.addNotification);
 
   const handleCopy = () => {
@@ -20,7 +23,11 @@ export function QuizCode({ quizCode }: Props) {
 
   return (
     <>
-      <div className="w-full p-2 bg-gray-300 border border-slate-700 rounded-md mt-2 relative">
+      <div
+        className={`w-full p-2 ${
+          variant === "slate" ? "bg-slate-700 text-white" : "bg-gray-300"
+        } border border-slate-700 rounded-md mt-2 relative`}
+      >
         <Text as="h3" category="subtitle" className="text-center">
           {quizCode}
         </Text>
@@ -28,7 +35,7 @@ export function QuizCode({ quizCode }: Props) {
         <button
           onClick={handleCopy}
           className="absolute right-1 top-2"
-          title="copiar"
+          title="copiar token"
         >
           <ClipboardIcon className="w-6 h-6" />
         </button>
