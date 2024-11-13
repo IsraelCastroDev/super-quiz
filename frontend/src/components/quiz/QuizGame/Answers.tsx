c191d45c517f69d87d382b372002e5d81127fe91
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-12 19:08:44 -0500

    renderizando Skeleton mientras se cargan las respuestas

--- frontend/src/components/quiz/QuizGame/Answers.tsx
+++ frontend/src/components/quiz/QuizGame/Answers.tsx
@@ -1,5 +1,5 @@
 import { getAnswers } from "@/api/quizAPI";
-import { Loader } from "@/components/ui";
+import { Skeleton } from "@/components/ui";
 import { Label } from "@/components/ui/Form";
 import { useQuery } from "@tanstack/react-query";
 
@@ -23,25 +23,34 @@ export function Answers({ question, selectedAnswer, onAnswerChange }: Props) {
     queryFn: () => getAnswers(question._id),
   });
 
-  if (isLoading) return <Loader />;
-
-  return answers ? (
-    <div>
-      {answers.map((answer) => (
-        <div key={answer._id} className="flex gap-x-2">
-          <input
-            type="radio"
-            name="answer"
-            id={`asnwer-${answer._id}`}
-            checked={selectedAnswer === answer._id}
-            onChange={() => onAnswerChange(answer._id)}
-            value={selectedAnswer}
-          />
-          <Label text={answer.title} htmlFor={`asnwer-${answer._id}`} />
+  return (
+    <>
+      {isLoading ? (
+        Array.from({ length: 5 }).map((_, index) => (
+          <div key={index} className="flex items-center gap-x-3">
+            <Skeleton className="w-3 h-3 rounded-full" />
+            <Skeleton className="w-80 h-3 rounded-sm" />
+          </div>
+        ))
+      ) : answers ? (
+        <div>
+          {answers.map((answer) => (
+            <div key={answer._id} className="flex gap-x-2">
+              <input
+                type="radio"
+                name="answer"
+                id={`asnwer-${answer._id}`}
+                checked={selectedAnswer === answer._id}
+                onChange={() => onAnswerChange(answer._id)}
+                value={selectedAnswer}
+              />
+              <Label text={answer.title} htmlFor={`asnwer-${answer._id}`} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  ) : (
-    "No se encontraron respuestas"
+      ) : (
+        "No se encontraron respuestas"
+      )}
+    </>
   );
 }
