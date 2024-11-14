2ffc7ceb15f9d295febf1fa4bb592107805cf019
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-14 14:21:46 -0500

    menu mobile cuando el usuario esta logeado

---
+++ frontend/src/components/Header/MenuMobileLoggedIn.tsx
@@ -0,0 +1,102 @@
+import {
+  Separator,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTrigger,
+  Text,
+} from "@/components/ui";
+import { UserAuthData } from "@/types";
+import {
+  ArrowRightCircleIcon,
+  PlusCircleIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+
+interface Props {
+  userAuth: UserAuthData;
+  handleLogout: () => void;
+}
+
+export function MonuMobileLoggedIn({ userAuth, handleLogout }: Props) {
+  return (
+    <div className="block md:hidden">
+      <Sheet>
+        <SheetTrigger asChild className="z-50">
+          <button className="outline-none">
+            <UserCircleIcon className="w-10 h-10" />
+          </button>
+        </SheetTrigger>
+
+        <SheetContent side={"right"} className="w-[80%] bg-gray-200 md:hidden">
+          <SheetHeader>
+            <Text as="h1" category="big">
+              ¡Hola {userAuth.name}!
+            </Text>
+          </SheetHeader>
+
+          <Separator className="bg-slate-900 my-3" />
+
+          <div className="space-y-3 mt-4">
+            <div className="flex flex-col items-start gap-2">
+              <div className="flex flex-col items-start gap-2 sm:hidden w-full">
+                <Text as="h2" category="subtitle">
+                  Ingresa un código
+                </Text>
+                <input className="font-semibold w-full bg-gray-300 rounded-md p-2 outline-none flex-1" />
+              </div>
+
+              <SheetClose asChild>
+                <Link to={"/perfil"} className="flex items-center gap-2">
+                  <UserCircleIcon className="w-8 h-8" />
+                  <Text as="h2" category="subtitle">
+                    Perfil
+                  </Text>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link to={"/quiz/crear"} className="flex items-center gap-2">
+                  <PlusCircleIcon className="w-8 h-8" />
+                  <Text as="h2" category="subtitle">
+                    Crear un Super Quiz
+                  </Text>
+                </Link>
+              </SheetClose>
+            </div>
+
+            <Separator className="bg-slate-900 my-3" />
+            <SheetClose asChild>
+              <button
+                onClick={() => handleLogout()}
+                className="flex items-center gap-2"
+              >
+                <ArrowRightCircleIcon className="w-8 h-8" />
+                <Text as="h2" category="subtitle">
+                  Cerrar sesión
+                </Text>
+              </button>
+            </SheetClose>
+          </div>
+
+          <SheetFooter className="mt-10">
+            <Text as="h1" category="body" className="font-semibold text-center">
+              Desarrollado por{" "}
+              <Link
+                to={"https://www.linkedin.com/in/juan-castro-chozo/"}
+                target="_blank"
+                rel="nooponer noreferrer"
+                className="underline"
+              >
+                Israel Castro
+              </Link>
+            </Text>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
