e6594ff83744fa7a91db3eb297bfc8cf372abb61
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-15 19:07:30 -0500

    usando brevo

--- backend/src/services/mail/mail.ts
+++ backend/src/services/mail/mail.ts
@@ -1,4 +1,5 @@
-import { transporter } from "../../config/nodemailer";
+import { brevo } from "../../config/brevoMail";
+import { apiInstance } from "../../config/brevoMail";
 
 interface IEmail {
   name: string;
@@ -7,33 +8,46 @@ interface IEmail {
 }
 
 export async function sendConfirmEmail(user: IEmail) {
-  const info = await transporter.sendMail({
-    from: "Super Trivia <admin@superquiz.com>",
-    to: user.email,
-    subject: "Super Quiz - Confirma tu cuenta",
-    text: "Confirma tu cuenta en Super Quiz",
-    html: `<p>Hola ${user.name}, solo estás a un paso de divertirte en Super Quiz, confirma tu cuenta</p>
-          <p>Visita el siguiente enlace:</>
-          <a href="${process.env.FRONTEND_URL}/confirmar-cuenta">Confirmar Cuenta</a>
-          <p>E ingresa el siguiente código: <b>${user.token}</b></p>
-          <p>Este token expira en aproximadamente 2 horas</>
-    `,
-  });
-  console.log(`Mensaje enviado ${info.messageId}`);
+  try {
+    const sendSmtEmail = new brevo.SendSmtpEmail();
+
+    sendSmtEmail.subject = "Confirma tu cuenta en Super Quiz";
+
+    sendSmtEmail.to = [{ email: user.email, name: user.name }];
+
+    sendSmtEmail.htmlContent = `<h1 style="border-bottom: 1px solid black;">Confirma tu cuenta - Super Quiz</h1><p>Hola ${user.name}, solo estás a un paso de divertirte en Super Quiz, confirma tu cuenta</p><p>Visita el siguiente enlace: </p><a href="${process.env.FRONTEND_URL}/confirmar-cuenta">Confirmar Cuenta</a><p>E ingresa el siguiente código: <b>${user.token}</b></p><p>Este token expira en aproximadamente 2 horas</>`;
+
+    sendSmtEmail.sender = {
+      email: "israelcastro1007@gmail.com",
+      name: "Israel Castro - Super Quiz",
+    };
+
+    const result = await apiInstance.sendTransacEmail(sendSmtEmail);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function sendResetPasswordEmail(user: IEmail) {
-  const info = await transporter.sendMail({
-    from: "Super Trivia <admin@superquiz.com>",
-    to: user.email,
-    subject: "Super Quiz - Recupera tu contraseña",
-    text: "Recupera tu contraseña en Super Quiz",
-    html: `<p>Hola ${user.name}, solo estás a un paso de recuparar tu contraseña en Super Quiz</p>
-          <p>Visita el siguiente enlace:</>
-          <a href="${process.env.FRONTEND_URL}/recuperar-cuenta/cambiar-password">Confirmar Cuenta</a>
-          <p>E ingresa el siguiente código: <b>${user.token}</b></p>
-          <p>Este token expira en aproximadamente 2 horas</>
-    `,
-  });
-  console.log(`Mensaje enviado ${info.messageId}`);
+  try {
+    const sendSmtEmail = new brevo.SendSmtpEmail();
+
+    sendSmtEmail.subject = "Super Quiz - Cambio de contraseña";
+
+    sendSmtEmail.to = [{ email: user.email, name: user.name }];
+
+    sendSmtEmail.htmlContent = `<p>Hola ${user.name}, solo estás a un paso de recuparar tu contraseña en Super Quiz</p><p>Visita el siguiente enlace:</><a href="${process.env.FRONTEND_URL}/recuperar-cuenta cambiar-password">Cambiar contraseña</a><p>E ingresa el siguiente código: <b>${user.token}</b></p><p>Este token expira en aproximadamente 2 horas</>
+    `;
+
+    sendSmtEmail.sender = {
+      email: "israelcastro1007@gmail.com",
+      name: "Super Quiz - Israel Castro",
+    };
+
+    const result = await apiInstance.sendTransacEmail(sendSmtEmail);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
