ebf78867ca54556ba8d6e728f7d697df9dbd0722
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-15 19:25:55 -0500

    ajustando estilos

--- frontend/src/components/home/HeroSection.tsx
+++ frontend/src/components/home/HeroSection.tsx
@@ -10,20 +10,29 @@ export function HeroSection({ userAuth }: Props) {
     <section className="flex flex-col items-center justify-center h-[calc(100vh-10rem)]">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] -z-10"></div>
 
-      <div className="max-w-[700px] mx-auto space-y-7">
+      <div className="max-w-[800px] w-full mx-auto space-y-7">
         <div className="flex flex-col gap-4 justify-center items-center">
-          <Text category="big" degraded={true} as="h2" className="text-center">
-            ¿Puedes superar a Super Quiz?
+          <Text
+            category="big"
+            degraded={true}
+            as="h2"
+            className="text-center text-balance"
+          >
+            Juega o crea tu propio Super Quiz.
           </Text>
-          <div className="px-0 md:px-10 flex justify-center">
-            <Text as="h3" category="subtitle" className="text-pretty">
+          <div className="flex justify-center">
+            <Text
+              as="h3"
+              category="subtitle"
+              className="text-pretty text-center"
+            >
               Elige entre diversas categorías, temas de tu agrado, obtén un
               puntaje y sube en el ranking de mejores jugadores.
             </Text>
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center justify-center gap-4 px-4">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-4">
           <Button to={"/"} variant="solid" size="lg">
             ¡Empieza a jugar ya!
           </Button>
